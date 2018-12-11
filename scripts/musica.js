cc17b78b87865d164b18d2e474575d5bbcf085d6
Alisson Nunes <alynva@gmail.com>
2018-12-11 12:20:46 -0200

    Refatoração de função de música

--- scripts/musica.js
+++ scripts/musica.js
@@ -4,6 +4,77 @@ class Musica {
 			throw TypeError("Esta class não pode ser instanciada.")
 	}
 
+	static onLoad() {
+		this._setupHandlers()
+		this.mostrarSugestoes()
+	}
+
+	static _setupHandlers() {
+		document.querySelector("#fm_song").addEventListener("submit", this.fmLoadPlaylistHandle)
+	}
+
+	static YTAPIReady() {
+		player = new YT.Player('player', {
+			// height: '1',
+			// width: '1',
+			events: {
+				'onReady': this._onPlayerReady,
+				'onStateChange': this._onPlayerStateChange,
+				'onError': this._onPlayerError.bind(this)
+			}
+		});
+	}
+
+	static _onPlayerReady() {
+		document.querySelector("#fm_song fieldset").disabled = false
+		player.setVolume(100)
+		player.setPlaybackQuality('small')
+	}
+
+	static _onPlayerStateChange(e) {
+		// document.querySelector("#btn_play_pause").value = e.data == YT.PlayerState.PLAYING ? "Pausar" : e.data == YT.PlayerState.BUFFERING ? "Carregando" : "Reproduzir"
+	
+		// document.querySelector("#btn_play_pause").disabled = e.data == YT.PlayerState.BUFFERING
+	}
+
+	static _onPlayerError(e) {
+		this.nextSongHandle()
+		switch (e.data) {
+			case 2:
+				throw Error("Erro no player do YouTube: A solicitação contém um valor de parâmetro inválido. Por exemplo, este erro ocorre se você especificar um ID de vídeo que não tem 11 caracteres, ou se o ID de vídeo contém caracteres inválidos, como pontos de exclamação ou asteriscos.")
+			case 5:
+				throw Error("Erro no player do YouTube: O conteúdo solicitado não pode ser reproduzido em um player HTML5, ou ocorreu outro erro relacionado ao player HTML5.")
+			case 100:
+				throw Error("Erro no player do YouTube: O vídeo solicitado não foi encontrado. Esse erro ocorrerá quando um vídeo tiver sido removido (por qualquer motivo) ou marcado como privado.")
+			case 101:
+			case 150:
+				throw Error("Erro no player do YouTube: O proprietário do vídeo solicitado não permite que ele seja reproduzido em players incorporados.")
+			default:
+				throw Error("Ocorrou um erro desconhecido no player do YouTube.")
+		}
+	}
+
+	static fmLoadPlaylistHandle() {
+		const ipt_element = document.querySelector("#ipt_playlist_url"),
+			url = new URL(ipt_element.value),
+			searchParams = new URLSearchParams(url.search),
+			playlist_id = searchParams.get('list'),
+			index_playing = searchParams.get('index'),
+			video_id = searchParams.get('v')
+
+		if (playlist_id) {
+			player.loadPlaylist({
+				list: playlist_id,
+				index: index_playing || 0
+			})
+			player.setLoop(true)
+		} else if (video_id) {
+			player.loadVideoById({
+				videoId: video_id
+			})
+		}
+	}
+
 	static _escolherSugestoes() {
 		const salvas = [
 			{
@@ -75,4 +146,22 @@ class Musica {
 			document.querySelector("#sugestoes").appendChild(li_element)
 		});
 	}
+
+	static prevSongHandle() {
+		player.previousVideo()
+	}
+	static playSongHandle() {
+		if (player.getPlayerState() != YT.PlayerState.PLAYING) {
+			player.playVideo()
+		}
+	}
+	static pauseSongHandle() {
+		if (player.getPlayerState() == YT.PlayerState.PLAYING) {
+			player.pauseVideo()
+		}
+	}
+	static nextSongHandle() {
+		player.nextVideo()
+	}
 }
+var player;
