fd5e4a2cbb975c73b5faa329e4875149731a9099
Alisson Nunes <alynva@gmail.com>
2018-12-28 20:48:37 -0200

    Aprimorado modularização dos scripts
    Resta rever como é definido os atalhos de música (hotkeys.js:L102-L105 e hotkeys.js:L123-L142)

--- scripts/hotkeys.js
+++ scripts/hotkeys.js
@@ -4,157 +4,157 @@ hotkeys.filter = function(event) {
 	return !(tagName.isContentEditable || tagName == 'INPUT' || tagName == 'SELECT' || tagName == 'TEXTAREA') || event.key == "Escape" || SS_Options.HotKeys.gravando
 }
 
-var SS_Options = SS_Options || {};
+var HotKeys = {};
 (function() {
-	this.HotKeys = {};
+	this.gravando = null
+
+	var atalhos = {},
+		novos_atalhos = {},
+		THIS = this
+
+	this.Handlers = {};
 	(function() {
-		this.gravando = null
+		this.iptAlterarAtalhoHandleOnFocusIn = e => {
+			THIS.gravando = e.target
+			e.target.classList.add("gravando")
+			hotkeys.setScope("gravando")
+		}
+		this.iptAlterarAtalhoHandleOnFocusOut = e => {
+			THIS.gravando = null
+			e.target.classList.remove("gravando")
+			hotkeys.setScope("config")
 
-		var atalhos = {},
-			novos_atalhos = {},
-			THIS = this
-
-		this.Handlers = {};
-		(function() {
-			this.iptAlterarAtalhoHandleOnFocusIn = e => {
-				THIS.gravando = e.target
-				e.target.classList.add("gravando")
-				hotkeys.setScope("gravando")
-			}
-			this.iptAlterarAtalhoHandleOnFocusOut = e => {
-				THIS.gravando = null
-				e.target.classList.remove("gravando")
-				hotkeys.setScope("config")
-
-				DefinirAtalhos.definirTodos()
-			}
-			
-			this.atalhoFecharConfigHandler = () => {
-				const details = document.querySelector("details")
-				if (details.hasAttribute("open")) {
-					document.querySelector("summary").click()
-					hotkeys.setScope('main')
-
-					window.location = "#"
-				}
-			}
-			this.atalhoAbrirConfigHandler = (focar = true) => {
-				const details = document.querySelector("details")
-				if (!details.hasAttribute("open")) {
-					document.querySelector("summary").click()
-					hotkeys.setScope('config')
-
-					// if (focar) focarNoElemento("input")
-				}
-			}
-			
-			this.atalhoMenuNavHandler = (link_id_ref) => {
-				if (THIS.gravando) return
-				if (hotkeys.getScope() == "main")
-					this.atalhoAbrirConfigHandler(false)
-				document.querySelector(`*[href='${link_id_ref}']`).click()
-				focarNoElemento(`${link_id_ref} input`)
-			}
-			const focarNoElemento = query => {
-				// Para o evento não ser passado pro input
-				setTimeout(() => document.querySelector(query).focus(), 0)
-			}
-		}).apply(this.Handlers)
-
-		this.init = () => {
-			hotkeys("*", 'gravando', capturarTeclas)
-			criarAtalhosPadrao()
 			DefinirAtalhos.definirTodos()
 		}
+		
+		this.atalhoFecharConfigHandler = () => {
+			const details = document.querySelector("details")
+			if (details.hasAttribute("open")) {
+				document.querySelector("summary").click()
+				hotkeys.setScope('main')
 
-		const capturarTeclas = e => {
-			if (THIS.gravando) {
-				THIS.gravando.value = hotkeys.getPressedKeyCodes().map(e => e = keyCodes[e]).join("+").toLowerCase()
-				atalhos[THIS.gravando.getAttribute("data-atalho")] = THIS.gravando.value
-				e.preventDefault()
+				window.location = "#"
 			}
 		}
+		this.atalhoAbrirConfigHandler = (focar = true) => {
+			const details = document.querySelector("details")
+			if (!details.hasAttribute("open")) {
+				document.querySelector("summary").click()
+				hotkeys.setScope('config')
 
-		const criarAtalhosPadrao = () => {
-			Object.defineProperty(atalhos, "_abrirConfig", { value: "o", writable: false, enumerable: true, configurable: false })
-			Object.defineProperty(atalhos, "_fecharConfig", { value: "esc", writable: false, enumerable: true, configurable: false })
+				// if (focar) focarNoElemento("input")
+			}
+		}
 		
-			Object.defineProperty(atalhos, "_detacarInfo", { value: "i", writable: false, enumerable: true, configurable: false })
-			Object.defineProperty(atalhos, "_detacarCont", { value: "c", writable: false, enumerable: true, configurable: false })
-			Object.defineProperty(atalhos, "_detacarMusi", { value: "m", writable: false, enumerable: true, configurable: false })
-			Object.defineProperty(atalhos, "_detacarTema", { value: "t", writable: false, enumerable: true, configurable: false })
-			Object.defineProperty(atalhos, "_detacarAtal", { value: "a", writable: false, enumerable: true, configurable: false })
+		this.atalhoMenuNavHandler = (link_id_ref) => {
+			if (THIS.gravando) return
+			if (hotkeys.getScope() == "main")
+				this.atalhoAbrirConfigHandler(false)
+			document.querySelector(`*[href='${link_id_ref}']`).click()
+			focarNoElemento(`${link_id_ref} input`)
+		}
+		const focarNoElemento = query => {
+			// Para o evento não ser passado pro input
+			setTimeout(() => document.querySelector(query).focus(), 0)
+		}
+	}).apply(this.Handlers)
+
+	this.init = () => {
+		hotkeys("*", 'gravando', capturarTeclas)
+		criarAtalhosPadrao()
+		DefinirAtalhos.definirTodos()
+	}
+
+	const capturarTeclas = e => {
+		if (THIS.gravando) {
+			THIS.gravando.value = hotkeys.getPressedKeyCodes().map(e => e = keyCodes[e]).join("+").toLowerCase()
+			atalhos[THIS.gravando.getAttribute("data-atalho")] = THIS.gravando.value
+			e.preventDefault()
+		}
+	}
+
+	const criarAtalhosPadrao = () => {
+		Object.defineProperty(atalhos, "_abrirConfig", { value: "o", writable: false, enumerable: true, configurable: false })
+		Object.defineProperty(atalhos, "_fecharConfig", { value: "esc", writable: false, enumerable: true, configurable: false })
+	
+		Object.defineProperty(atalhos, "_detacarInfo", { value: "i", writable: false, enumerable: true, configurable: false })
+		Object.defineProperty(atalhos, "_detacarCont", { value: "c", writable: false, enumerable: true, configurable: false })
+		Object.defineProperty(atalhos, "_detacarMusi", { value: "m", writable: false, enumerable: true, configurable: false })
+		Object.defineProperty(atalhos, "_detacarTema", { value: "t", writable: false, enumerable: true, configurable: false })
+		Object.defineProperty(atalhos, "_detacarAtal", { value: "a", writable: false, enumerable: true, configurable: false })
+	
+		Object.defineProperty(atalhos, "_tocarMusica", { value: "ctrl+down", writable: false, enumerable: true, configurable: false })
+		Object.defineProperty(atalhos, "_pausarMusica", { value: "ctrl+down", writable: false, enumerable: true, configurable: false })
+		Object.defineProperty(atalhos, "_musicaAnterior", { value: "ctrl+left", writable: false, enumerable: true, configurable: false })
+		Object.defineProperty(atalhos, "_proximaMusica", { value: "ctrl+right", writable: false, enumerable: true, configurable: false })
+	}
+
+	var DefinirAtalhos = {};
+	(function() {
+		this.definirTodos = () => {
+			// Escopo ALL
+			definirAtalhoMenuNav(atalhos._detacarInfo, 'all', "#config_info", "#ipt_keyword_edit_info")
+			definirAtalhoMenuNav(atalhos._detacarCont, 'all', "#config_count", "#ipt_keyword_edit_count")
+			definirAtalhoMenuNav(atalhos._detacarMusi, 'all', "#config_song", "#ipt_keyword_edit_music")
+			definirAtalhoMenuNav(atalhos._detacarTema, 'all', "#config_tema", "#ipt_keyword_edit_theme")
+			definirAtalhoMenuNav(atalhos._detacarAtal, 'all', "#config_atal", "#ipt_keyword_edit_hotkey")
+			
+			// TODO: Rever como é definido os atalhos de música 
+			// definirAtalhoTocarMusica()
+			// definirAtalhoPausarMusica()
+			// definirAtalhoMusicaAnterior()
+			// definirAtalhoProximaMusica()
 		
-			Object.defineProperty(atalhos, "_tocarMusica", { value: "ctrl+down", writable: false, enumerable: true, configurable: false })
-			Object.defineProperty(atalhos, "_pausarMusica", { value: "ctrl+down", writable: false, enumerable: true, configurable: false })
-			Object.defineProperty(atalhos, "_musicaAnterior", { value: "ctrl+left", writable: false, enumerable: true, configurable: false })
-			Object.defineProperty(atalhos, "_proximaMusica", { value: "ctrl+right", writable: false, enumerable: true, configurable: false })
+			// Escopo MAIN
+			definirAtalhoAbreConfig()
+		
+			// Escopo CONFIG
+			definirAtalhoFechaConfig()
+		}
+		
+		function definirAtalhoMenuNav(key, scope, link_id_ref, ipt_id_ref) {
+			hotkeys(key, scope, () => THIS.Handlers.atalhoMenuNavHandler(link_id_ref))
+			atualizarAtalhoNoInput(ipt_id_ref, key)
+		}
+		
+		const atualizarAtalhoNoInput = (selector, key) => {
+			document.querySelector(selector).value = String(key).toLowerCase()
+		}
+		
+		// TODO: Rever como é definido os atalhos de música 
+		// const definirAtalhoTocarMusica = () => {
+		// 	const key = atalhos.tocarMusica || atalhos._tocarMusica
+		// 	hotkeys(key, SS_Options.Music.Handlers.playSongHandler)
+		// 	atualizarAtalhoNoInput("#ipt_keyword_play_music", key)
+		// }
+		// const definirAtalhoPausarMusica = () => {
+		// 	const key = atalhos.pausarMusica || atalhos._pausarMusica
+		// 	hotkeys(key, SS_Options.Music.Handlers.pauseSongHandler)
+		// 	atualizarAtalhoNoInput("#ipt_keyword_pause_music", key)
+		// }
+		// const definirAtalhoMusicaAnterior = () => {
+		// 	const key = atalhos.musicaAnterior || atalhos._musicaAnterior
+		// 	hotkeys(key, SS_Options.Music.Handlers.prevSongHandler)
+		// 	atualizarAtalhoNoInput("#ipt_keyword_prev_music", key)
+		// }
+		// const definirAtalhoProximaMusica = () => {
+		// 	const key = atalhos.proximaMusica || atalhos._proximaMusica
+		// 	hotkeys(key, SS_Options.Music.Handlers.nextSongHandler)
+		// 	atualizarAtalhoNoInput("#ipt_keyword_next_music", key)
+		// }
+		const definirAtalhoAbreConfig = () => {
+			const key = atalhos.abrirConfig || atalhos._abrirConfig
+			hotkeys(key, "main", () => THIS.Handlers.atalhoAbrirConfigHandler(false))
+			atualizarAtalhoNoInput("#ipt_keyword_open_config", key)
+		}
+		const definirAtalhoFechaConfig = () => {
+			const key = atalhos.fecharConfig || atalhos._fecharConfig
+			hotkeys(key, "config", THIS.Handlers.atalhoFecharConfigHandler)
+			atualizarAtalhoNoInput("#ipt_keyword_close_config", key)
+			document.querySelector("#btn_exit_config ~ span").innerText = String(key).toUpperCase()
 		}
 
-		var DefinirAtalhos = {};
-		(function() {
-			this.definirTodos = () => {
-				// Escopo ALL
-				definirAtalhoMenuNav(atalhos._detacarInfo, 'all', "#config_info", "#ipt_keyword_edit_info")
-				definirAtalhoMenuNav(atalhos._detacarCont, 'all', "#config_count", "#ipt_keyword_edit_count")
-				definirAtalhoMenuNav(atalhos._detacarMusi, 'all', "#config_song", "#ipt_keyword_edit_music")
-				definirAtalhoMenuNav(atalhos._detacarTema, 'all', "#config_tema", "#ipt_keyword_edit_theme")
-				definirAtalhoMenuNav(atalhos._detacarAtal, 'all', "#config_atal", "#ipt_keyword_edit_hotkey")
-				
-				definirAtalhoTocarMusica()
-				definirAtalhoPausarMusica()
-				definirAtalhoMusicaAnterior()
-				definirAtalhoProximaMusica()
-			
-				// Escopo MAIN
-				definirAtalhoAbreConfig()
-			
-				// Escopo CONFIG
-				definirAtalhoFechaConfig()
-			}
-			
-			function definirAtalhoMenuNav(key, scope, link_id_ref, ipt_id_ref) {
-				hotkeys(key, scope, () => THIS.Handlers.atalhoMenuNavHandler(link_id_ref))
-				atualizarAtalhoNoInput(ipt_id_ref, key)
-			}
-			
-			const atualizarAtalhoNoInput = (selector, key) => {
-				document.querySelector(selector).value = String(key).toLowerCase()
-			}
-			
-			const definirAtalhoTocarMusica = () => {
-				const key = atalhos.tocarMusica || atalhos._tocarMusica
-				hotkeys(key, SS_Options.Music.Handlers.playSongHandler)
-				atualizarAtalhoNoInput("#ipt_keyword_play_music", key)
-			}
-			const definirAtalhoPausarMusica = () => {
-				const key = atalhos.pausarMusica || atalhos._pausarMusica
-				hotkeys(key, SS_Options.Music.Handlers.pauseSongHandler)
-				atualizarAtalhoNoInput("#ipt_keyword_pause_music", key)
-			}
-			const definirAtalhoMusicaAnterior = () => {
-				const key = atalhos.musicaAnterior || atalhos._musicaAnterior
-				hotkeys(key, SS_Options.Music.Handlers.prevSongHandler)
-				atualizarAtalhoNoInput("#ipt_keyword_prev_music", key)
-			}
-			const definirAtalhoProximaMusica = () => {
-				const key = atalhos.proximaMusica || atalhos._proximaMusica
-				hotkeys(key, SS_Options.Music.Handlers.nextSongHandler)
-				atualizarAtalhoNoInput("#ipt_keyword_next_music", key)
-			}
-			const definirAtalhoAbreConfig = () => {
-				const key = atalhos.abrirConfig || atalhos._abrirConfig
-				hotkeys(key, "main", () => THIS.Handlers.atalhoAbrirConfigHandler(false))
-				atualizarAtalhoNoInput("#ipt_keyword_open_config", key)
-			}
-			const definirAtalhoFechaConfig = () => {
-				const key = atalhos.fecharConfig || atalhos._fecharConfig
-				hotkeys(key, "config", THIS.Handlers.atalhoFecharConfigHandler)
-				atualizarAtalhoNoInput("#ipt_keyword_close_config", key)
-				document.querySelector("#btn_exit_config ~ span").innerText = String(key).toUpperCase()
-			}
+	}).apply(DefinirAtalhos)
+}).apply(HotKeys)
 
-		}).apply(DefinirAtalhos)
-	}).apply(this.HotKeys)
-
-}).apply(SS_Options)
+export default HotKeys
---
+++ scripts/ss_options.js
@@ -0,0 +1,215 @@
+import HotKeys from "./hotkeys.js"
+import Music from "./music.js"
+
+var SS_Options = SS_Options || {};
+(function() { // SS_Options namespace
+	this.HotKeys = HotKeys
+	this.Music = Music
+
+	var THIS = this
+	var count_text = "Começaremos em breve!"
+	function init() {
+		document.querySelectorAll("form").forEach(element => {
+			element.addEventListener("submit", e => e.preventDefault())
+		})
+		
+		document.querySelector("#fm_info").addEventListener("change", THIS.Handlers.fmInfoHandler)
+		document.querySelector("#fm_time").addEventListener("submit", THIS.Handlers.fmTimeHandler)
+		document.querySelector("#fm_add_time").addEventListener("submit", THIS.Handlers.fmAddTimeHandler)
+		document.querySelector("#fm_theme").addEventListener("change", THIS.Handlers.fmThemeHandler)
+	
+		document.querySelector("#s_count").innerText = count_text
+		document.querySelector("#s_count").setAttribute("data-text", count_text)
+	
+		document.querySelector("#btn_exit_config").addEventListener("click", THIS.HotKeys.Handlers.atalhoFecharConfigHandler)
+		document.querySelector("#btn_open_config").addEventListener("click", THIS.HotKeys.Handlers.atalhoAbrirConfigHandler)
+		THIS.HotKeys.Handlers.atalhoAbrirConfigHandler()
+	
+		document.querySelectorAll("#fm_atal input").forEach(element => {
+			element.addEventListener("focusin", THIS.HotKeys.Handlers.iptAlterarAtalhoHandleOnFocusIn)
+			element.addEventListener("focusout", THIS.HotKeys.Handlers.iptAlterarAtalhoHandleOnFocusOut)
+		})
+		THIS.HotKeys.init()
+		THIS.Music.init()
+	}
+
+	this.Handlers = {};
+	(function() { // Handlers namespace
+		this.onDocumentLoadHandler = function () {
+			init()
+		}
+
+		var SocialMediaHandlers = {};
+		(function() { // SocialMediaHandlers namespace
+			this.socialMediaHandler = (e, keys) => {
+				const parent = e.target.parentElement
+				const index = Array(...parent.parentElement.children).indexOf(parent)
+				const emptySibling = (e.target.previousElementSibling && e.target.previousElementSibling.value == "") || (e.target.nextElementSibling && e.target.nextElementSibling.value == "")
+				const li_query = `#${keys[e.target.className]} .index-${index}`
+				if (e.target.value == "") {
+					if (emptySibling) parent.parentElement.removeChild(parent)
+			
+					const li = document.querySelector(li_query)
+					if (li) {
+						li.parentElement.removeChild(li)
+					}
+				}
+				else {
+					if (e.target.value != "" && parent.nextElementSibling == null) {
+						const row = parent.cloneNode(true)
+						row.firstElementChild.value = ""
+						row.lastElementChild.value = ""
+						parent.insertAdjacentElement('afterEnd', row)
+					}
+					let li = document.querySelector(li_query)
+					if (li === null) {
+						li = document.createElement("li")
+						li.classList.add(`index-${index}`)
+						document.querySelector(`#${keys[e.target.className]}`).appendChild(li)
+					}
+					li.innerText = getShortUrl(e.target.value)
+					li.classList.add(getWebSiteName(e.target.value))
+				}
+			}
+			function getShortUrl(url) {
+				const pattern = /(\w+\.(?:com|net|tv|com\.br|me).*)/ig
+				const result = pattern.exec(url)
+				return result[1]
+			}
+			function getWebSiteName(url) {
+				const pattern = /(\w+).(?:com|net|tv|com\.br|me)/ig
+				const result = pattern.exec(url)
+				return result[1]
+			}			
+		}).apply(SocialMediaHandlers)
+		this.fmInfoHandler = function(e) {
+			e.preventDefault()
+			
+			const keys = {
+				'ipt_org': 'h_org',
+				'ipt_title': 'h_title',
+				'ipt_social_media': 'ul_social_medias',
+				'ipt_social_media_secundary': 'ul_social_medias_secundary',
+				'ipt_link_hotpage': 'a_hotpage'
+			}
+		
+			switch (e.target.className) {
+				case 'ipt_social_media':
+				case 'ipt_social_media_secundary':
+					SocialMediaHandlers.socialMediaHandler(e, keys);
+					break
+		
+				default:
+					document.querySelector(`#${keys[e.target.id]}`).innerHTML = e.target.value
+					document.querySelector(`#${keys[e.target.id]}`).setAttribute("data-text", e.target.value)
+			}
+
+		}
+
+		var CountDown = {};
+		(function() { // CountDown namespace
+			this.countInterval = null
+			this.endTimeMessage = null
+
+			this.stopCountDown = () => {
+				clearInterval(this.countInterval)
+				this.countInterval = null
+				document.querySelector("#btn_start_stop_time").value = "Começar"
+				document.querySelector("#ipt_time_2_end").disabled = false;
+				this.endTimeMessage = setTimeout(() => {
+					document.querySelector("#s_count").innerText = count_text
+					document.querySelector("#s_count").setAttribute("data-text", count_text)
+				}, 5000)
+			}
+			this.countDown = end => {
+				const now = new Date(),
+				left = {
+					hours: Math.floor(Math.abs(end - now) / 36e5).toFixed(0).padStart(2, 0),
+					minutes: (Math.floor(Math.abs(end - now) / 6e4) % 60).toFixed(0).padStart(2, 0),
+					seconds: (Math.floor(Math.abs(end - now) / 1e3) % 60).toFixed(0).padStart(2, 0)
+				}
+				
+				const text = `<b>${left.hours}</b>:${left.minutes}:<i>${left.seconds}</i>`
+				document.querySelector("#s_count").innerHTML = text
+				document.querySelector("#s_count").setAttribute("data-text", text.replace(/<\/?[^>]+(>|$)/g, ""))
+				
+				if (end <= now) this.stopCountDown()
+			}
+		}).apply(CountDown)
+		this.fmTimeHandler = e => {
+			e.preventDefault()
+			
+			if (!CountDown.countInterval) {
+				const now = new Date(),
+					ipt_data = e.target.ipt_time_2_end.value.split(":"),
+					ipt_hour = Number(ipt_data[0]),
+					ipt_minute = Number(ipt_data[1]),
+					estimed_end = new Date(new Date().setHours(ipt_hour, ipt_minute, 0))
+				
+				if (now > estimed_end) {
+					estimed_end.setDate(now.getDate()+1)
+				}
+				
+				clearTimeout(CountDown.endTimeMessage)
+				CountDown.countInterval = setInterval(CountDown.countDown, 10, estimed_end)
+				
+				e.target.ipt_time_2_end.disabled = true;
+				document.querySelector("#btn_start_stop_time").value = "Parar"
+			} else {
+				CountDown.stopCountDown()
+			}
+		}
+		this.fmAddTimeHandler = () => {
+			clearTimeout(CountDown.endTimeMessage) // Precisa parar o timeout aqui pois quando chamar o stopCountDown, ele criará um novo timeout e o anterior continuará sendo executado mesmo que o novo seja parado dentro do fmTimeHandle
+			CountDown.stopCountDown()
+			
+			const ipt_data = document.querySelector("#ipt_time_2_add").value.split(":"),
+			ipt_hour = Number(ipt_data[0]),
+			ipt_minute = Number(ipt_data[1]),
+			previous_end_data = document.querySelector("#ipt_time_2_end").value.split(":"),
+			previous_hour = previous_end_data[0] != "" ? Number(previous_end_data[0]) : new Date().getHours(),
+			previous_minute = previous_end_data[1] ? Number(previous_end_data[1]) : new Date().getMinutes(),
+			new_hour = (previous_hour + ipt_hour + Math.floor((previous_minute + ipt_minute) / 60)) % 24,
+			new_minute = (previous_minute + ipt_minute) % 60,
+			new_end_data = `${String(new_hour).padStart(2, 0)}:${String(new_minute).padStart(2, 0)}`
+			
+			document.querySelector("#ipt_time_2_add").value = ""
+			document.querySelector("#ipt_time_2_end").value = new_end_data
+			document.querySelector("#btn_start_stop_time").click()
+		}
+
+		this.fmThemeHandler = e => {
+			const theme_name = e.target.value,
+				style_id = "theme_style",
+				script_id = "theme_script"
+		
+			let style = document.head.querySelector(`#${style_id}`),
+				script = document.head.querySelector(`#${script_id}`)
+		
+			if (theme_name != "none") {
+				if (style === null) {
+					style = document.createElement("link")
+					style.setAttribute("rel", "stylesheet")
+					style.setAttribute("type", "text/css")
+					style.setAttribute("id", style_id)
+					document.head.appendChild(style)
+				}
+		
+				style.setAttribute("href", `themes/${theme_name}/${theme_name}.css`)
+				
+				if (script === null) {
+					script = document.createElement("script")
+					script.setAttribute("id", script_id)
+					document.head.appendChild(script)
+				}
+		
+				script.setAttribute("src", `themes/${theme_name}/${theme_name}.js`)
+			} else {
+				if (style !== null) style.parentElement.removeChild(style)
+				if (script !== null) script.parentElement.removeChild(script)
+			}
+		}
+	}).apply(this.Handlers)
+}).apply(SS_Options)
+
+export default SS_Options
